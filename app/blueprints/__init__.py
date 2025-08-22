0e2c55a0154b09b4a3a61ea5032696172b7e5c2f
Patrick BAYO <bayopatrick2@gmail.com>
2025-08-22 13:49:34 +0200

    add tailwind templates

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 from app.blueprints.auth import auth_bp
 
 def register_blueprints(app: Flask):
-    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(auth_bp, url_prefix='/')
